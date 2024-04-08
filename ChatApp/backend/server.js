9b864c2a95c48a7c9ef8ab4ecce9f09cd3acf755
chamindu77 <chamindunipun99@gmail.com>
2024-04-08 10:07:56 +0530

    complete backend part

--- ChatApp/backend/server.js
+++ ChatApp/backend/server.js
@@ -3,14 +3,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 
-// File import
+// Routes
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+// Database Part
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // Variables
 const app = express();
-app.use
 const PORT = process.env.PORT || 5000;
 
 // .env config
@@ -27,8 +29,10 @@ dotenv.config();
 app.use(express.json()); // to parse the incoming requests with JSON payload (from req.body)
 app.use(cookieParser());
 
+// Routes Paths
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
 
 
 
