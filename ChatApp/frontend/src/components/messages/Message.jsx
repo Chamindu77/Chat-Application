ec24263541746acf3d5c45a07b2654e01ec482e1
chamindu77 <chamindunipun99@gmail.com>
2024-04-20 07:24:20 +0530

    20/04/2024

--- ChatApp/frontend/src/components/messages/Message.jsx
+++ ChatApp/frontend/src/components/messages/Message.jsx
@@ -8,7 +8,7 @@ const Message = () => {
                 </div>
                     
             </div>
-            <div className={'chat-bubble text-white [bg-blue-500' }>Hi! What is upp ?</div>
+            <div className={'chat-bubble text-white bg-blue-500' }>Hi! What is upp ?</div>
             <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
         </div>
     
